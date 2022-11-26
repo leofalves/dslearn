3cef8e755bb916123bff04980ac6baad7dcc38c8
Leo Fabiano Alves <leo.fabiano@gmail.com>
2022-11-26 11:09:05 -0300

    Course entity

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -15,3 +15,6 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Javascript', 'https://i0.wp.com/www.clickguarulhos.com.br/wp-content/uploads/2021/09/20210910-javacurso.jpg?fit=1024%2C683&ssl=1', 'https://blog.raleduc.com.br/wp-content/uploads/2016/06/aaasd.jpg');
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Oracle PL-SQL', 'https://arquivo.devmedia.com.br/cursos/imagem/curso_1456.jpg', 'http://www.mastertraining.com.br/wp-content/uploads/2013/02/oracle_black.jpg');
