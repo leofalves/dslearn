6a93d1dd43971446f8a11e09c82852bb5d8212f4
Leo Fabiano Alves <leo.fabiano@gmail.com>
2022-11-29 20:36:49 -0300

    Resource Entity

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Enums/ResourceType.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dslearnbds.entities.Enums;
+
+public enum ResourceType {
+	
+	LESSON_ONLY,
+	LESSON_TASK,
+	FORUM,
+	EXTERNAL_LINK;
+
+}
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -22,3 +22,8 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Oracle PL-SQL', 'ht
 INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES ('JS 2022 - BLACK FRIDAY', TIMESTAMP WITH TIME ZONE '2022-11-01T03:00:01Z', TIMESTAMP WITH TIME ZONE '2022-11-30T02:59:59Z', 1);
 INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES ('JS 2022 - CHRISTMAS', TIMESTAMP WITH TIME ZONE '2022-12-01T03:00:01Z', TIMESTAMP WITH TIME ZONE '2022-12-30T02:59:59Z', 1);
 INSERT INTO tb_offer(edition, start_moment, end_moment, course_id) VALUES ('ORCL 2022 - BLACK FRIDAY', TIMESTAMP WITH TIME ZONE '2022-11-01T03:00:01Z', TIMESTAMP WITH TIME ZONE '2022-11-30T02:59:59Z', 2);
+
+INSERT INTO tb_resource(title, description, position, img_uri, type, offer_id) VALUES ('Trilha Javascript Vanilla','Trilha principal do curso JS', 1, 'https://i0.wp.com/www.clickguarulhos.com.br/wp-content/uploads/2021/09/20210910-javacurso.jpg?fit=1024%2C683&ssl=1', 1, 1);
+INSERT INTO tb_resource(title, description, position, img_uri, type, offer_id) VALUES ('Forum','Tire suas dúvidas', 2, 'https://i0.wp.com/www.clickguarulhos.com.br/wp-content/uploads/2021/09/20210910-javacurso.jpg?fit=1024%2C683&ssl=1', 2, 1);
+INSERT INTO tb_resource(title, description, position, img_uri, type, offer_id) VALUES ('Aulas ao vivo','Lives exclusivas para a turma', 3, 'https://i0.wp.com/www.clickguarulhos.com.br/wp-content/uploads/2021/09/20210910-javacurso.jpg?fit=1024%2C683&ssl=1', 0, 1);
+
