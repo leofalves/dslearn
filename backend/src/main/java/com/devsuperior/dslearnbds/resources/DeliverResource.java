c82f6b4b506ddcfaf3e116abe19816e95a9c6dbc
Leo Fabiano Alves <leo.fabiano@gmail.com>
2022-12-23 11:48:59 -0300

    Preauthorize methods

--- backend/src/main/java/com/devsuperior/dslearnbds/resources/DeliverResource.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/DeliverResource.java
@@ -2,6 +2,7 @@ package com.devsuperior.dslearnbds.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +19,9 @@ public class DeliverResource {
 	@Autowired
 	private DeliverService service;
 	
+	@PreAuthorize("hasAnyRole('ADMIN','INSTRUCTOR')")	// Pre-autoriza o endpoint somente para os roles ADMIN e INSTRUCTOR --> Esta annotation pode ser incluída na camada de serviço
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody DeliverRevisionDto dto){
+	public ResponseEntity<Void> saveRevision(@PathVariable Long id, @RequestBody DeliverRevisionDto dto){
 		service.saveRevision(id, dto);
 		return ResponseEntity.noContent().build();
 	}
